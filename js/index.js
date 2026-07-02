239e2372aac4331bba20592c40a16fb7912eec68
swrtan <58186570+swrtan@users.noreply.github.com>
2026-07-03 00:18:03 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Sana bir hediyem var Jalem`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
